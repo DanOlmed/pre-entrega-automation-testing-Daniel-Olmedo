040f3550f1c1f7e0420c522ea51b2c9ab1d59a88
DanOlmedo <oxdeo85@gmail.com>
2025-11-26 22:47:26 -0300

    Actualizacion de casos de test, reqres api, implementacionde logger

--- run_tests_files.py
+++ run_tests_files.py
@@ -2,8 +2,11 @@ import pytest
 
 test_files =[
     "tests/test_login.py",
+    "tests/test_login_faker.py",
     "tests/test_inventary.py",
-    "tests/test_badge.py"
+    "tests/test_badge.py",
+    "tests/test_badge_json.py",
+    "tests/test_api_reqres.py"
 ]
 
 #argumentos para ejecutar las pruebas = archivos de prueba + reporte html + verbosidad
